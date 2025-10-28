2d5c6cf295015c32a919affa45842a84e6d9ca60
MARIA GRAZIELE OTAVIO SAMPAIO <202508741211@alunos.unifanor.edu.br>
2025-10-28 09:21:40 -0300

    mensagem

---
+++ python/condicionais/condicionalcomposto.py
@@ -0,0 +1,6 @@
+numeroUsuario= int(input(" Digite um numero:"))
+if numeroUsuario==10:
+    print(f"usuario digitou o numero{numeroUsuario}")
+    
+else:
+    print(" Voçê não acertou o numero escolhido pelo sistema.")
---
+++ python/condicionais/condicionalsimples.py
@@ -0,0 +1,10 @@
+
+numero = 10 
+
+
+# if numero == 10:
+# print("" Este é o numero escolhido pelo usuario")
+
+numeroUsuario= int(input(" Digite um numero:"))
+if numeroUsuario==10:
+    print(f"usuario digitou o numero{numeroUsuario}")
